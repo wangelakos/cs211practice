4dc21b93450b6007186bebc358773c66c1e44129
William Angelakos <83235537+wangelakos@users.noreply.github.com>
2022-11-14 22:12:23 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  gitPractice
+//
+//  Created by William Angelakos on 11/13/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
