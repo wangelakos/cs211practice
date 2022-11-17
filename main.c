845679b1c36b824330ddf36e9ec8bb9ad8b4ce05
William Angelakos <83235537+wangelakos@users.noreply.github.com>
2022-11-17 15:04:39 +0000

    Saving changes with new function

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void wangel3_dkhan24() {
+  printf("\nWilliam Angelakos and Daniyal Khan\n");
+  printf("We learned how to push a program to GitHub. We practiced the github commands that we have learned in class\n");
+  }
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    wangel3_dkhan24();
+    
+    return 0;
 }
